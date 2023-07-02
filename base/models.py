961fc3c1fafa20488a35882cd14e189af0270c5c
Noamblander <127669824+Noamblander@users.noreply.github.com>
2023-07-02 13:18:24 +0300

    Update models.py

--- base/models.py
+++ base/models.py
@@ -15,9 +15,13 @@ class Product(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     desc = models.CharField(max_length=50,null=True,blank=True)
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=7,decimal_places=2, null=True, blank=True)
     amount = models.IntegerField()
     createdTime=models.DateTimeField(auto_now_add=True)
- 
+
+    def save(self, *args, **kwargs):
+        self.price = self.price * self.amount
+        super(Order, self).save(*args, **kwargs)  # Call the "real" save() method.
+
     def __str__(self):
            return f'{self.desc}  '
